face2781b5da2cbabfa6d5a124d094c37d2fd6b6
Cairo Braz Costa <cairo.costa@animaeducacao.com.br>
2024-05-20 21:52:37 -0300

    finalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "9999999", modelo: "Nokia A", imei: "88888", memoria: 10);
+nokia.Ligar();
+nokia.InstalarAplicativo("Calendario");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone: ");
+Smartphone iphone = new Iphone(numero: "3333333", modelo: "Iphone A", imei: "222222", memoria: 20);
+iphone.Ligar();
+iphone.InstalarAplicativo("Autenticador");
